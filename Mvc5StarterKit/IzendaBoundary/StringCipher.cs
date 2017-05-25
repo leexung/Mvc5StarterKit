a099256898a805774561dcb773cbba6ccb24a006
Triet Pham <trietphamnm@gmail.com>
2017-05-25 17:31:15 +0700

    Add StringCipher class
    Add StringCipher class that use native .net encryption instead of using Izenda encryption which is obfuscated

---
+++ Mvc5StarterKit/IzendaBoundary/StringCipher.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Mvc5StarterKit.IzendaBoundary
+{
+    public static class StringCipher
+    {
+        private static readonly AesCryptoServiceProvider Crypto = new AesCryptoServiceProvider();
+
+        public static string Encrypt(string raw, string key)
+        {
+            byte[] inBlock = Encoding.UTF8.GetBytes(raw);
+            ICryptoTransform xfrm = Crypto.CreateEncryptor();
+            byte[] outBlock = xfrm.TransformFinalBlock(inBlock, 0, inBlock.Length);
+            return Convert.ToBase64String(outBlock);
+        }
+
+        public static string Decrypt(string encrypted, string key)
+        {
+            byte[] inBytes = Convert.FromBase64String(encrypted);
+            ICryptoTransform xfrm = Crypto.CreateDecryptor();
+            byte[] outBlock = xfrm.TransformFinalBlock(inBytes, 0, inBytes.Length);
+
+            return Encoding.UTF8.GetString(outBlock);
+        }
+    }
+}
