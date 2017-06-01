e785781c0d876fe6677287dc263e347fc337752f
Binh Tran <binhtran@kms-technology.com>
2017-06-01 15:08:33 +0700

    Add report/view/{id} route for send exporting subscription report

---
+++ Mvc5StarterKit/Controllers/DashboardController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mvc5StarterKit.Controllers
+{
+    public class DashboardController : Controller
+    {
+        // GET: DashboardViewer
+        public ActionResult DashboardViewer()
+        {
+            throw new NotImplementedException("This feature is developing");
+        }
+    }
+}
