d88f68e7a749fd11a87ff6ad6c10c84671626a5e
Binh Tran <binhtran@kms-technology.com>
2017-10-02 15:43:48 +0700

    Update util script to auto hide loading spinner when element izenda-root is rendered

--- Mvc5StarterKit/Scripts/izenda.utils.js
+++ Mvc5StarterKit/Scripts/izenda.utils.js
@@ -1,26 +1,23 @@
 ﻿$(function () {
+    // select the target node
+    var target = document.querySelector('#izenda-root');
 
     var observer = new MutationObserver(function (mutations) {
         mutations.forEach(function (mutation) {
-            //console.log($(mutation.removedNodes)); // <<-- includes text nodes
-
             $("#progressLoader").hide();
             $("#progressLoaderText").hide();
-            console.log("> Loader removed...")
             $(mutation.removedNodes).each(function (value, index) {
                 if (this.nodeType === 1) {
-                    console.log(this)
+                    console.log(this);
                 }
             });
         });
     });
 
+    // configuration of the observer:
     var config = { attributes: true, childList: true, characterData: true };
-
-    var divId = "'#izenda-root'";
-
-    if (document.getElementById(divId))
-    {
-        observer.observe($(divId)[0], config);
+    if (target) {
+        // pass in the target node, as well as the observer options
+        observer.observe(target, config);
     }
 });
